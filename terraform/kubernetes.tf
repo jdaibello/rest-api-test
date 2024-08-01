64e91561daa5f2ac34ec3457a38330445c7369db
João Pedro <jpedrodaibello1999@outlook.com>
2024-08-01 14:26:03 -0300

    feat(iac): add regcred secret resources accross envs

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -17,3 +17,51 @@ resource "kubernetes_namespace" "local_cluster_namespace" {
     name = "rest-api-test-${each.key}-ns"
   }
 }
+
+resource "kubernetes_secret" "regcred_secret_non_prod" {
+  for_each = { for ns in local.non_prod_cluster_namespaces : ns => ns }
+
+  metadata {
+    name = "regcred"
+    namespace = "rest-api-test-${each.key}-ns"
+  }
+
+  data = {
+      ".dockerconfigjson" = jsonencode({
+      auths = {
+        "https://index.docker.io/v1/" = {
+          username = var.dockerhub_username,
+          password = var.dockerhub_password,
+          email = var.dockerhub_email,
+          auth = base64encode("${var.dockerhub_username}:${var.dockerhub_password}")
+        }
+      }
+    })
+  }
+
+  type = "kubernetes.io/dockerconfigjson"
+}
+
+resource "kubernetes_secret" "regcred_secret_prod" {
+  for_each = { for ns in local.prod_cluster_namespaces : ns => ns }
+
+  metadata {
+    name = "regcred"
+    namespace = "rest-api-test-${each.key}-ns"
+  }
+
+  data = {
+    ".dockerconfigjson" = jsonencode({
+      auths = {
+        "https://index.docker.io/v1/" = {
+          username = var.dockerhub_username,
+          password = var.dockerhub_password,
+          email = var.dockerhub_email,
+          auth = base64encode("${var.dockerhub_username}:${var.dockerhub_password}")
+        }
+      }
+    })
+  }
+
+  type = "kubernetes.io/dockerconfigjson"
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,18 @@ variable "aws_region" {
   type        = string
   default     = "us-east-2"
 }
+
+variable "dockerhub_email" {
+  description = "Email address to login on Docker Hub Registry."
+  type        = string
+}
+
+variable "dockerhub_username" {
+  description = "Docker Hub Registry username."
+  type        = string
+}
+
+variable "dockerhub_password" {
+  description = "Password to login on Docker Hub Registry. Prefer to use a Personal Access Token."
+  type        = string
+}
