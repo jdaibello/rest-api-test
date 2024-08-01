45aa0d9ce0cf581d128a740829321003a397b133
João Pedro <jpedrodaibello1999@outlook.com>
2024-08-01 07:21:58 -0300

    feat(ci): remove update_configs.sh

--- update_configs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Load Environment Variables
-source .env
-
-# Log Environment Variables
-echo "COMMIT_HASH_SHORT=${COMMIT_HASH_SHORT}"
-echo "KUSTOMIZE_PATH=${KUSTOMIZE_PATH}"
-echo "DOCKER_IMAGE_TAG=${DOCKER_IMAGE_TAG}"
