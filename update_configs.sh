2457692176bbe25cfdc99bc62df360fd86c4c498
João Pedro <jpedrodaibello1999@outlook.com>
2024-08-01 07:15:06 -0300

    feat(ci): use custom env instead GITHUB_ENV

--- update_configs.sh
+++ update_configs.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-# Load Environment Variables from GITHUB_ENV
-source $GITHUB_ENV
+# Load Environment Variables
+source .env
 
 # Log Environment Variables
 echo "COMMIT_HASH_SHORT=${COMMIT_HASH_SHORT}"
 echo "KUSTOMIZE_PATH=${KUSTOMIZE_PATH}"
 echo "DOCKER_IMAGE_TAG=${DOCKER_IMAGE_TAG}"
-
-# Using sed to modify the variables
-# sed -i "s/__COMMIT_HASH_SHORT__/${COMMIT_HASH_SHORT}/g" path/to/your/config/file
-# sed -i "s/__KUSTOMIZE_PATH__/${KUSTOMIZE_PATH}/g" path/to/your/config/file
-# sed -i "s/__DOCKER_IMAGE_TAG__/${DOCKER_IMAGE_TAG}/g" path/to/your/config/file
