2a401d4dd953b5712ed42bb08c3c9b0bcb4d7fea
João Pedro <jpedrodaibello1999@outlook.com>
2024-08-01 22:46:22 -0300

    feat: uncomment AWS S3 resources

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,29 +1,29 @@
-# resource "aws_s3_bucket" "tfstate_remote_storage" {
-#   bucket = "test-joao-daibello-backend-terraform-state"
+resource "aws_s3_bucket" "tfstate_remote_storage" {
+  bucket = "test-joao-daibello-backend-terraform-state"
 
-#   tags = {
-#     Project = "backend"
-#   }
-# }
+  tags = {
+    Project = "backend"
+  }
+}
 
-# resource "aws_s3_bucket_versioning" "tfstate_storage_versioning" {
-#   bucket = aws_s3_bucket.tfstate_remote_storage.id
-#   expected_bucket_owner = data.aws_caller_identity.current.account_id
+resource "aws_s3_bucket_versioning" "tfstate_storage_versioning" {
+  bucket = aws_s3_bucket.tfstate_remote_storage.id
+  expected_bucket_owner = data.aws_caller_identity.current.account_id
 
-#   versioning_configuration {
-#     status = "Enabled"
-#   }
-# }
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
 
-# data "aws_iam_policy_document" "tfstate_bucket_policy" {
-#   statement {
-#     actions   = ["s3:CreateBucket"]
-#     resources = [aws_s3_bucket.tfstate_remote_storage.arn]
-#   }
+data "aws_iam_policy_document" "tfstate_bucket_policy" {
+  statement {
+    actions   = ["s3:CreateBucket"]
+    resources = [aws_s3_bucket.tfstate_remote_storage.arn]
+  }
 
-#   statement {
-#     actions   = ["s3:GetObject", "s3:PutObject"]
-#     resources = ["${aws_s3_bucket.tfstate_remote_storage.arn}/terraform.tfstate"]
-#     effect    = "Allow"
-#   }
-# }
+  statement {
+    actions   = ["s3:GetObject", "s3:PutObject"]
+    resources = ["${aws_s3_bucket.tfstate_remote_storage.arn}/terraform.tfstate"]
+    effect    = "Allow"
+  }
+}
