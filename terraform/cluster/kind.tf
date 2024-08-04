77e74f801a4c933a08ee1633eb6b29883ddadc30
João Pedro <jpedrodaibello1999@outlook.com>
2024-08-04 09:27:55 -0300

    feat: uncomment extra_port_mappings attribute

--- terraform/cluster/kind.tf
+++ terraform/cluster/kind.tf
@@ -22,14 +22,14 @@ resource "kind_cluster" "backend_cluster" {
       content {
         role = node.value
 
-        # dynamic "extra_port_mappings" {
-        #   for_each = node.value == "control-plane" ? [1] : []
+        dynamic "extra_port_mappings" {
+          for_each = node.value == "control-plane" ? [1] : []
 
-        #   content {
-        #     container_port = 80
-        #     host_port      = 80
-        #   }
-        # }
+          content {
+            container_port = 80
+            host_port      = 80
+          }
+        }
       }
     }
   }
