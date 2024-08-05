f193197d3bc8657467eac2083e7c74b5b4a1e340
João Pedro <jpedrodaibello1999@outlook.com>
2024-08-05 17:31:09 -0300

    chore(api): add cors package

--- server/server.js
+++ server/server.js
@@ -1,16 +1,18 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-app.use('/', require('./route/postsRoute'));
+app.use("/", require("./route/postsRoute"));
 app.use(function (error, req, res, next) {
-	if (error.message === 'Post already exists') {
-		return res.status(409).send(e.message);
-	}
-	if (error.message === 'Post not found') {
-		return res.status(404).send(e.message);
-	}
-	res.status(500).send(error.message);
+  if (error.message === "Post already exists") {
+    return res.status(409).send(e.message);
+  }
+  if (error.message === "Post not found") {
+    return res.status(404).send(e.message);
+  }
+  res.status(500).send(error.message);
 });
 
 app.listen(3000);
