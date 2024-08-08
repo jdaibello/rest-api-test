2fcf9031915508540ffcc825c3f9eb47dcd4f03f
João Pedro <jpedrodaibello1999@outlook.com>
2024-08-08 14:40:17 -0300

    feat(docker): update healtcheck route

--- server/route/healthcheckRoute.js
+++ server/route/healthcheckRoute.js
@@ -3,16 +3,15 @@ const router = express.Router();
 
 router.get("/healthcheck", async function (req, res, next) {
   const healthcheck = {
-      uptime: process.uptime(),
-      message: 'OK',
-      timestamp: Date.now()
-    };
-    try {
-      res.send(healthcheck);
-    } catch (error) {
-      healthcheck.message = error;
-      res.status(503).send();
-    }
+    message: "OK",
+    timestamp: Date.now(),
+  };
+  try {
+    res.send(healthcheck);
+  } catch (error) {
+    healthcheck.message = error;
+    res.status(503).send();
+  }
 });
 
 module.exports = router;
